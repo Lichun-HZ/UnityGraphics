712e7e24730f737d54171266cec43caf28c69181
lichun <lcbiotech@126.com>
2018-03-09 19:02:06 +0800

    添加MeshBake Demo

---
+++ Assets/Samples/MeshBake/CombineMeshes.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CombineMeshes : MonoBehaviour
+{
+    public bool MergeMaterial = false;
+
+    void Start()
+    {
+        //获取MeshRender;  
+        MeshRenderer[] meshRenders = GetComponentsInChildren<MeshRenderer>();
+
+        //材质;  
+        Material[] mats;
+
+        if (MergeMaterial)
+        {
+            mats = new Material[1];
+            for (int i = 0; i < meshRenders.Length; i++)
+            {
+                mats[i] = meshRenders[i].sharedMaterial;
+                break;
+            }
+        }
+        else
+        {
+            mats = new Material[meshRenders.Length];
+            for (int i = 0; i < meshRenders.Length; i++)
+            {
+                mats[i] = meshRenders[i].sharedMaterial;
+            }
+        }
+
+        //合并Mesh;  
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = transform.worldToLocalMatrix * meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+        }
+
+        MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter mf = gameObject.AddComponent<MeshFilter>();
+        mf.mesh = new Mesh();
+        mf.mesh.CombineMeshes(combine, MergeMaterial, true);
+        gameObject.SetActive(true);
+        mr.sharedMaterials = mats;
+    }
+
+}
---
+++ Assets/Samples/MeshBake/HG_UnLit_Dye_Transparent.shader
@@ -0,0 +1,73 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "HeroGo/General/UnLit/HG_Unlit_Dye_Transparent"
+{
+	Properties
+	{
+		_MainTex("Base (RGB) Trans (A)", 2D) = "white" {}
+		_DyeColor("Dye color",Color) = (1.0,1.0,1.0,1.0)
+		_ColorStrength("Color Strength",Range(0, 1)) = 0.3
+		[HideInInspector]_AlphaFactor("Alpha factor",Range(0,1)) = 1.0
+	}
+
+	SubShader
+	{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+		LOD 100
+
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_fog
+
+			#include "UnityCG.cginc"
+
+			struct appdata_t 
+			{
+				float4 vertex : POSITION;
+				float2 texcoord : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				half2 texcoord : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			fixed4 _DyeColor;
+			fixed _AlphaFactor;
+			half _ColorStrength;
+
+			v2f vert(appdata_t v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.texcoord = TRANSFORM_TEX(v.texcoord, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.texcoord) * _DyeColor;
+				col.rgb = lerp(col.rgb, col.rgb * normalize(col.rgb), _ColorStrength);
+				col.a *= _AlphaFactor;
+
+				UNITY_APPLY_FOG(i.fogCoord, col);
+
+				return col;
+			}
+
+			ENDCG
+		}
+	}
+
+}
